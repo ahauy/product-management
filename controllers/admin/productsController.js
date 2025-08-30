93e0c9ba99aa4fdb95b5624bfcce7eb470a09ee8
ahauy <vutuanhau.haui@gmail.com>
2025-08-30 10:36:19 +0700

    Update Product - Lấy ra danh sách sản phẩm

--- controllers/admin/productsController.js
+++ controllers/admin/productsController.js
@@ -1,5 +1,13 @@
-module.exports.index = (req, res) => {
+const Products = require('../../models/products.model')
+
+
+module.exports.index = async (req, res) => {
+  const products = await Products.find({
+    deleted: false
+  })
+  console.log(products)
   res.render('admin/pages/products/index.pug', {
     title: 'Trang danh sách sản phẩm',
+    products: products,
   })
 }
