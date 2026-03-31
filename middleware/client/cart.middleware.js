0af9ab49f8d57f03933a68ef75bcb2f5b39ea0b6
ahauy <vutuanhau.haui@gmail.com>
2026-04-01 02:19:44 +0700

    Fix: .pug

--- middleware/client/cart.middleware.js
+++ middleware/client/cart.middleware.js
@@ -13,7 +13,13 @@ module.exports.cardId = async (req, res, next) => {
     })
   } else {
     const cart = await Cart.findOne({_id: req.cookies.cartId})
-    res.locals.miniCart = cart;
+    if (!cart) {
+      // Nếu không tìm thấy giỏ hàng trong DB
+      res.locals.miniCart = { totalQuantity: 0 }; 
+    } else {
+      // Tính toán và gán giỏ hàng như bình thường
+      res.locals.miniCart = cart; 
+    }
   }
 
   next();
