47a2bd6a4b9561aef6a286cb23a4a5f2d7a1fcde
ahauy <vutuanhau.haui@gmail.com>
2026-04-01 02:04:31 +0700

    Fix multer upload directory for Vercel-1

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,14 +1,20 @@
+// config/multer.config.js
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('./cloudinary.config');
 
+// Cấu hình nơi lưu trữ trên Cloudinary
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  destination: function (req, file, cb) {
+    // Nếu chạy trên Vercel thì dùng '/tmp', nếu ở local thì dùng tạm './' hoặc '/tmp' đều được
+    cb(null, '/tmp') 
+  },
+  cloudinary,
   params: {
-    folder: 'MyClothes',
+    folder: 'MyClothes', // Tên folder lưu ảnh trên Cloudinary
     allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 module.exports = upload;
